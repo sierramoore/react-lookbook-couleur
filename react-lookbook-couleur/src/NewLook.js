2c4d0b80b9304097ba3c5ddf755c8fcd8e2fb2ce
Kait Mikitin <kait.mikitin@gmail.com>
2018-02-15 07:02:56 -0600

    got rid of db dir, scraper working

--- react-lookbook-couleur/src/NewLook.js
+++ react-lookbook-couleur/src/NewLook.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import cheerio from 'cheerio';
 
 class NewLook extends Component {
+	scrapeImages = (e) => {
+		e.preventDefault()
+		const link = e.currentTarget.previousSibling.value
+		console.log(link, typeof link)
+
+		const $ = cheerio.load(link)
+		console.log($)
+		console.log('--------------------------------------')
+		const arrayOfImg = $('img')
+		console.log(arrayOfImg)
+	}
 	render() {
 		return (
 			<div>
@@ -9,8 +21,7 @@ class NewLook extends Component {
 					Copy and paste link here:
 					<br/>
 					<input type="url" placeholder="https://..."/>
-					<br/>
-					<button>Save Link and Find Images</button>
+					<button onClick={this.scrapeImages}>Save Link and Find Images</button>
 				</form>
 			</div>
 			)
